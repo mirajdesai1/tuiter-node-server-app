86dc199c3f420b20d4266eba363b220da5b40fe6
Miraj Desai <mirajdesai01@gmail.com>
2023-04-01 17:34:18 -0400

    default tuit values

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -6,6 +6,10 @@ const createTuit = (req, res) => {
   newTuit._id = (new Date()).getTime() + '';
   newTuit.likes = 0;
   newTuit.liked = false;
+  newTuit.dislikes = 0;
+  newTuit.replies = 0;
+  newTuit.retuits = 0;
+  newTuit.image = 'nasa.png';
   tuits.push(newTuit);
   res.json(newTuit);
 }
